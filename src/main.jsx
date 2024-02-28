bd331aa7f3be21768f1545c3c74d56229595800d
Jonathan Araldi <jonathan.araldi@outlook.com>
2024-02-28 14:11:32 +0100

    Refactorisation des routes dans le fichier "Main.jsx" | Modification de l'architecture du projet avec ajout du dossier "layout" dans "src"

--- src/main.jsx
+++ src/main.jsx
@@ -1,47 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import Header from '../src/components/header/Header.jsx';
-import Footer from '../src/components/footer/Footer.jsx';
+import Header from './layout/header/Header.jsx';
+import Footer from './layout/footer/Footer.jsx';
+import Main_content from './layout/main_content/Main_content.jsx';
 import './main.scss';
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, RouterProvider, Outlet} from "react-router-dom";
+
+const LayoutHeaderFooter = () => {
+  return (
+    <>
+      <Header />
+      <Main_content>
+        <Outlet />
+      </Main_content>
+      <Footer />
+    </>
+  );
+};
 
 const router = createBrowserRouter([
-  /* ========== Page : Accueil ========== */
   {
-    path: "/",
-    element:(
-      <>
-        <App />
-      </>
-    ),
-
-    /* ========== Page : Erreur 404 ========== */
-    errorElement: <h1>404 not found</h1>
-  },
-
-  /* ========== Page : Logement ========== */
-  {
-    path:"/fiche-logement",
-    element: (
-      <>
-        <Header />
-        <h1>Nos Appartements</h1>
-        <Footer />
-      </>
-    )
-  },
-
-  /* ========== Page : À propos ========== */
-  {
-    path:"/about",
-    element: (
-      <>
-        <Header />
-        <h1>À propos</h1>
-        <Footer />
-      </>
-    ),
+    errorElement: <h1>404 not found</h1>, /* ========== Page : Erreur 404 ========== */
+    element: <LayoutHeaderFooter />,
+    children: [
+      {
+        /* ========== Page : Accueil ========== */
+        path: "/",
+        element: <App />
+      },
+      {
+        /* ========== Page : Logement ========== */
+        path:"/fiche-logement",
+        element: (
+          <h1>Nos Appartements</h1>
+        )
+      },
+      {
+        /* ========== Page : À propos ========== */
+        path:"/about",
+        element: (
+          <h1>À propos</h1>
+        )
+      }
+    ]
   },
 ]);
 
